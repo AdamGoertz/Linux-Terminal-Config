47f9e88bca7a17dc44fe91e1b5a6cbc2848760f3
goertab1 <Adam.Goertz@jhuapl.edu>
2022-02-25 16:30:57 -0500

    Back up old config files on setup

--- setup_sh.bash
+++ setup_sh.bash
@@ -1,5 +1,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OLD_CONFIG_DIR="old_configs"
+DATETIME=$(date +%F__%H_%M_%S)
 
 sudo apt install -y tmux xclip vim 
+
+mkdir -p $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+
+mv ~/.tmux.conf $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+mv ~/.bash_aliases $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
 
 ln -s $SCRIPT_DIR/.tmux.conf $SCRIPT_DIR/.bash_aliases ~
