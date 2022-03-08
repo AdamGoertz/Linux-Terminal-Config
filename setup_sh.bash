88825a193596efd2504587bb31ffde38b7d63711
goertab1 <Adam.Goertz@jhuapl.edu>
2022-03-08 10:41:13 -0500

    Extract config files and change setup_sh to loop over all config files

--- setup_sh.bash
+++ setup_sh.bash
@@ -1,13 +1,19 @@
+#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 OLD_CONFIG_DIR="old_configs"
+CURRENT_CONFIG_DIR="configs"
 DATETIME=$(date +%F__%H_%M_%S)
+
+shopt -s dotglob
 
 sudo apt install -y tmux xclip vim 
 
 mkdir -p $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
 
-mv ~/.tmux.conf $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
-mv ~/.bash_aliases $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
-mv ~/.vimrc $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+for FILE in $CURRENT_CONFIG_DIR/*; do
+        [ -e "$FILE" ] || continue
+        echo "Updating: $FILE..."
+        mv ~/$FILE $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+        ln -s $SCRIPT_DIR/$CURRENT_CONFIG_DIR/$FILE ~
+done
 
-ln -s $SCRIPT_DIR/.tmux.conf $SCRIPT_DIR/.bash_aliases $SCRIPT_DIR/.vimrc ~
