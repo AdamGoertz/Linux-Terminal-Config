0fdc5de21073b06d3d973c0d601081c359114d23
AdamGoertz <adambgoertz@gmail.com>
2022-07-15 02:21:04 +0000

    Add vimdiff color scheme and fix copy-paste in tmux conf

--- configs/.vimrc
+++ configs/.vimrc
@@ -1,11 +1,12 @@
 set ruler
-set cursorline
 set relativenumber
 set mouse=a
 set title
 set autoindent
 set expandtab
 set smarttab
+set softtabstop=4
+set shiftwidth=4
 
 if &diff
         colorscheme diffcolorscheme
--- setup_sh.bash
+++ setup_sh.bash
@@ -6,7 +6,7 @@ DATETIME=$(date +%F__%H_%M_%S)
 
 shopt -s dotglob
 
-sudo apt install -y tmux xclip vim xterm
+sudo apt update && sudo apt install -y tmux xclip vim xterm x11-xserver-utils
 
 mkdir -p $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
 
@@ -16,8 +16,17 @@ for FILE in $CURRENT_CONFIG_DIR/*; do
         [ -e "$FILE" ] || continue
         FILE_BASE=$(basename $FILE)
         echo "Updating: $FILE_BASE..."
-        mv ~/$FILE_BASE $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
-        ln -s $SCRIPT_DIR/$FILE ~
+        FILE_HOME="~/$FILE_BASE"
+
+        if [ -L $FILE_HOME ]; then
+                rm $FILE_HOME
+        fi
+
+        if [ -e $FILE_HOME ]; then
+                mv $FILE_HOME $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+        fi
+
+        ln -s $SCRIPT_DIR/$CURRENT_CONFIG_DIR/$FILE ~
 done
 
 xrdb -merge ~/.Xresources
