d574da51d0f200cd64489121b8072fb03774fc95
Adam Goertz <adambgoertz@gmail.com>
2024-02-15 22:28:34 -0500

    Add files to gitignore

--- setup_sh.bash
+++ setup_sh.bash
@@ -21,25 +21,23 @@ mkdir -p $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
 # TODO: Add config directories like .vim
 
 for FILE in $CURRENT_CONFIG_DIR/*; do
-        FILE_BASE=$(basename $FILE)
-        echo "Updating: $FILE_BASE..."
-        FILE_HOME="$HOME/$FILE_BASE"
-        echo "Checking state of $FILE_HOME"
+    FILE_BASE=$(basename $FILE)
+    echo "Updating: $FILE_BASE..."
+    FILE_HOME="$HOME/$FILE_BASE"
+    echo "Checking state of $FILE_HOME"
 
-	find ~ -xtype l -delete
+    if [ -L "$FILE_HOME" ]; then
+    echo "Removing existing symlink: $FILE_HOME"
+            unlink $FILE_HOME
+    fi
 
-        if [ -L "$FILE_HOME" ]; then
-		echo "Removing existing symlink: $FILE_HOME"
-                unlink $FILE_HOME
-        fi
-
-        if [ -e "$FILE_HOME" ]; then
-		echo "Moving existing file: $FILE_HOME to $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME"
-                mv $FILE_HOME $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
-        fi
+    if [ -e "$FILE_HOME" ]; then
+    echo "Moving existing file: $FILE_HOME to $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME"
+            mv $FILE_HOME $SCRIPT_DIR/$OLD_CONFIG_DIR/$DATETIME
+    fi
 
 	echo "Creating symlink: $SCRIPT_DIR/$FILE"
-        ln -s $SCRIPT_DIR/$FILE ~
+    ln -s $SCRIPT_DIR/$FILE ~
 done
 
 xrdb -merge ~/.Xresources
