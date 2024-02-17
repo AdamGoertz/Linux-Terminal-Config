0082fb15c3b7f593b2bb508d444adab05d8cf544
Adam Goertz <adambgoertz@gmail.com>
2024-02-17 13:17:15 -0500

    Update LSP remappings

--- configs/.config/nvim/lua/config/lazy_init.lua
+++ configs/.config/nvim/lua/config/lazy_init.lua
@@ -137,8 +137,8 @@ require("lazy").setup({
             vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
             vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
             vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-            vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-            vim.keymap.set('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+            vim.keymap.set('i', '<C-o>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+            vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
           end
         })
 
