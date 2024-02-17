62f8bb9f4cb139c71ec211ec69b1bf350a905937
Adam Goertz <adambgoertz@gmail.com>
2024-02-17 15:50:05 -0500

    Add tab swap remappings

--- configs/.config/nvim/lua/config/remap.lua
+++ configs/.config/nvim/lua/config/remap.lua
@@ -37,3 +37,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+for i = 1,9 do
+    vim.keymap.set("n", string.format("\\%d", i), string.format("%dgt", i))
+end
